6af5dff36ac098df253c5d76b46a8d9511389342
John Glennan <glennan20@gmail.com>
2021-08-05 18:33:00 -0400

    removed extra command

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('interactionCreate', interaction => {
-	console.log(interaction);
-});
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
-client.on('interactionCreate', interaction => {
-	console.log(interaction);
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
 });
 
 // login to Discord with your app's token;
