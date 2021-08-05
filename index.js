a1b4988f551fdc8830246bfa94953b256436977e
john glennan <glennan20@gmail.com>
2021-08-05 19:39:59 -0400

    fixed intents

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const { Client, Intents } = require('discord.js');
 
 // create a new Discord client
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client();
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -12,13 +12,12 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
+client.on("message", msg => {
+	if (msg.content === "ping") {
+		console.log(msg)
+	  msg.reply("pong");
 	}
-});
+  })
 
 // login to Discord with your app's token;
 dotenv.config();
