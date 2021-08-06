e7a1bf66efc676371ed2c4b8a45609cf4f911a8e
john glennan <glennan20@gmail.com>
2021-08-05 20:48:14 -0400

    added joke api to fetch jokes and return them in the server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const badwordsArray = require('badwords/array')
+const fetch = require('node-fetch');
 
 // require the needed discord.js classes
 const { Client, Intents } = require('discord.js');
@@ -18,16 +19,22 @@ client.on("message", msg => {
 	if (newMsg === "ping") {
 		console.log(msg)
 	  msg.reply("pong");
-	} else if (newMsg === "shit") {
-		msg.reply("You don't say that")
-	} else if (newMsg === "fuck") {
-		msg.reply("I'm Walking here :fire:")
-	} else if (newMsg === "Fuck!") {
-		msg.reply("dude stop it :D")
 	} else if (badwordsArray.indexOf(newMsg) > -1) {
-		console.log(newMsg)
-		msg.reply(`${newMsg} is a bad word`)
-	}		
+		fetch(`https://v2.jokeapi.dev/joke/Any?contains=${newMsg}`).then(r => r.json()).then(joke => {
+			if (joke.setup) {
+				msg.reply(joke.setup)
+				setTimeout(() => {
+					msg.reply(joke.delivery)
+				}, 7000);
+			} else if (joke.joke) {
+				msg.reply(joke.joke)
+			} else {
+				msg.reply(`${newMsg} is a bad word`)
+			}
+		})
+	} else {
+		console.log('this isn't working')
+	}
   })
 
 // login to Discord with your app's token;
